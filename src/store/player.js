3e4fb7c1d55e9be77f1ad568f65a0e3a7ae1f4a6
Darren <darren.hu3@gmail.com>
2021-07-20 15:35:53 -0400

    player store update

--- src/store/player.js
+++ src/store/player.js
@@ -1,13 +1,30 @@
 import axios from "axios";
 
+const GET_ALL_PLAYER = "GET_ALL_PLAYER";
+
 const GET_PLAYER = "GET_PLAYER";
 
 const defaultPlayer = "";
+
+const getAllPlayer = (data) => ({
+  type: GET_ALL_PLAYER,
+  data: data,
+});
 
 const getPlayer = (data) => ({
   type: GET_PLAYER,
   data: data,
 });
+
+export const fetchAllPlayer = () => {
+  return async (dispatch) => {
+    try {
+      const { data } = await axios.get("/api/player");
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+};
 
 export const fetchPlayer = (userid) => {
   return async (dispatch) => {
